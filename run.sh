3826e106e9f8b75f8f216dfeda93ad31794dde50
Robert Moller <33206528+roberthmoller@users.noreply.github.com>
2022-06-26 16:48:34 +0000

    Added run script and graphviz dependency

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+serverAddress="https://roberthmoller-volatility-visualizer-vw4r7wqq3wv49-8501.githubpreview.dev"
+serverPort=80
+
+streamlit run --browser.serverPort $serverPort --browser.serverAddress $serverAddress *Home*
